8dbf97e889ff113729a24f70a101fcc38545159e
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-13 11:43:11 -0300

    Adição do construtor
    Commit contendo o primeiro construtor para o objeto restaurante

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,15 @@
 class restaurante:
-    nome =''
-    categoria =''
-    ativo = False
+    def __init__(self, nome, categoria):   
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 
-restaurante_praca = restaurante()
-restaurante_praca.nome = 'praça do sushi'
-restaurante_praca.categoria = 'japones'
-restaurante_pizza = restaurante()
+restaurante_praca = restaurante('Praça do shushi', 'japones')
+restaurante_pizza = restaurante('Pizza do Jao', 'pizzaria')
 
 restaurantes = [restaurante_pizza,restaurante_praca]
 
-print(restaurantes)
+print(restaurante_pizza)
+print(restaurante_praca)
