94392c412fc008174b486591f0511647e59f4e39
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-13 13:12:51 -0300

    Criação da função de Alteração
    Commit da criação da função de alterar o estado do entre ativo e inativo

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -8,18 +8,20 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
-
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'nome do restaurante'.ljust(25)} | {'categoria'.ljust(25)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
     @property
     def ativo(self):
         return '✅' if self._ativo else '❌'
     
+    def alterar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça do shushi', 'japones')
-restaurante_praca._nome = 'Praça do japa'
+restaurante_praca.alterar_estado()
 restaurante_pizza = Restaurante('Pizza do Jao', 'pizzaria')
 
 Restaurante.listar_restaurantes()
