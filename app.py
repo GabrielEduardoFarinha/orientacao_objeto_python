a6a9d055040ccbe112ca9bc28e77a7e27f8fd28f
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-13 14:10:58 -0300

    tratamento de exceção na avaliação
    Commit contendo o tratamento de exceção na avaliação limitando a nota entre 0 a 5

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ restaurante_0 = Restaurante('praça do sushi', 'japonesa')
 restaurante_1 = Restaurante('pizza do jao', 'pizzaria')
 restaurante_2 = Restaurante('Quentinhas do PP', 'popular')
 
-restaurante_0.receber_avaliacao('Junin', 10)
-restaurante_0.receber_avaliacao('', 9)
-restaurante_0.receber_avaliacao('Christakis', 2)
+restaurante_0.receber_avaliacao('Junin', 5)
+restaurante_0.receber_avaliacao('PD', 5)
+restaurante_0.receber_avaliacao('Christakis', 7)
 
 def main():
     Restaurante.listar_restaurantes()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -29,13 +29,14 @@ class Restaurante:
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota):
-        avaliacao = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
     
     @property
     def nota_restaurante(self):
         if not self._avaliacao:
-            return 0
+            return 'não avaliado'
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
