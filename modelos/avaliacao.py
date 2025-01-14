ee69076a5e317ea203b2ec42d8e64187965c2661
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-14 10:01:35 -0300

    Ajuste no avaliações
    Commit com ajuste no avaliacoes para evitar conflito no codigo

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,6 +1,4 @@
 class Avaliacao:
     def __init__(self, cliente, nota):
-        if not 1 <= nota <= 5:
-            raise ValueError("A nota deve estar entre 1 e 5.")
         self._cliente = cliente
         self._nota = nota
