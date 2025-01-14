d97ab17b4760656cb932b6924a7e8006b7044085
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-14 11:33:19 -0300

    Criando a lista cardapio e abstração
    Commit criando a lista de cardapio e utilizando abstração.

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from modelos.cardapio.prato import Prato
 
 restaurante_0 = Restaurante('praça do sushi', 'japonesa')
 bebida_1 = Bebida('guaravita', 2.00, '300ml')
+bebida_1.aplicar_desconto()
 prato_1 = Prato('lamen', 49.00, 'Lamen de tonkostu')
+prato_1.aplicar_desconto()
 
+restaurante_0.adicionar_no_cardapio(bebida_1)
+restaurante_0.adicionar_no_cardapio(prato_1)
 
 def main():
-    print(prato_1)
-    print(bebida_1)
+    restaurante_0.exibir_cardapio
 
 
 
